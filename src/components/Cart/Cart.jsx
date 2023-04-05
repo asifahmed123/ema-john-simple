51b342dbcd3389cb5a98bc0355894689e05a972c
asifahmed123 <ahmedasif092@gmail.com>
2023-04-05 12:03:03 +0600

    implemented delete from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,16 +4,23 @@ import "./Cart.css"
 const Cart = ({ cart }) => {
      let total = 0;
      let shippingCharge = 0
+     let quantity = 0;
+
      for (const product of cart) {
-          total = total + product.price;
+          // if(product.quantity === 0){
+          //      product.quantity = 1
+          // }
+          product.quantity = product.quantity || 1;
+          total = total + product.price * product.quantity;
           shippingCharge = shippingCharge + product.shipping;
+          quantity = quantity + product.quantity
      }
      const tax = total * 5 / 100;
      const grandTotal = total + shippingCharge + tax;
      return (
           <div className='cart'>
                <h4>Cart Container</h4>
-               <p>Selected Items: {cart.length}</p>
+               <p>Selected Items: {quantity}</p>
                <p>Total Price:${total}</p>
                <p>Total Shipping Charge: ${shippingCharge}</p>
                <p>Tax:${tax.toFixed(2)}</p>
